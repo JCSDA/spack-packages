61629f2b9074a10b8bf29f6ca9e209598155ffd1
Evan Parker <eparker@ucar.edu>
2025-11-10 23:34:04 -0700

    Update package.py
    Pull in changes from https://github.com/spack/spack-packages/pull/2322/files

--- repos/spack_repo/builtin/packages/netcdf_c/package.py
+++ repos/spack_repo/builtin/packages/netcdf_c/package.py
@@ -358,7 +358,8 @@ class CMakeBuilder(AnyBuilder, cmake.CMakeBuilder):
             self.define("ENABLE_LARGE_FILE_SUPPORT", True),
             self.define_from_variant("NETCDF_ENABLE_LOGGING", "logging"),
         ]
-        if "+mpi" in self.pkg.spec:
+        # Note: NC_EXTRA_DEPS is not subject to the v4.9.3 name change.
+        if "+mpi" in self.pkg.spec and "platform=windows" not in self.pkg.spec:
             base_cmake_args.append(self.define("NC_EXTRA_DEPS", "mpi"))
         if "+parallel-netcdf" in self.pkg.spec:
             base_cmake_args.append(self.define("ENABLE_PNETCDF", True))
