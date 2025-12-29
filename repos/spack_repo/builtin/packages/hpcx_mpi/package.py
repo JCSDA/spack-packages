00930cf0058a31374383faa0590cbe5512a57402
Rick Grubin <richard.grubin@noaa.gov>
2025-12-29 21:43:13 +0000

    Add property libs for mpi spec

--- repos/spack_repo/builtin/packages/hpcx_mpi/package.py
+++ repos/spack_repo/builtin/packages/hpcx_mpi/package.py
@@ -40,15 +40,15 @@ class HpcxMpi(Package):
         env.prepend_path("LD_LIBRARY_PATH", prefix.lib)
         env.set("OPAL_PREFIX", prefix)
 
-    def setup_dependent_build_environment(
-        self, env: EnvironmentModifications, dependent_spec: Spec
-    ) -> None:
-        self.make_base_environment(self.prefix, env)
-
     @property
     def libs(self):
         libraries = ["libmpi"]
         return find_libraries(libraries, root=self.prefix, shared=True, recursive=True)
+
+    def setup_dependent_build_environment(
+        self, env: EnvironmentModifications, dependent_spec: Spec
+    ) -> None:
+        self.make_base_environment(self.prefix, env)
 
     def setup_run_environment(self, env: EnvironmentModifications) -> None:
         self.make_base_environment(self.prefix, env)
